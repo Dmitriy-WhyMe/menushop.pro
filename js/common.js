c86d6f8109bec9ead307d73a94ef4acf34c40865
Dmitriy <dima.ivahnoff@mail.ru>
2021-08-27 18:37:56 +0300

    mailss

--- js/common.js
+++ js/common.js
@@ -136,7 +136,7 @@
 		 
 		function sendAjaxForm(result_form, ajax_form, url) {
 		    $.ajax({
-		        url:     ./mail/check.php, //url страницы (action_ajax_form.php)
+		        url:     './mail/check.php', //url страницы (action_ajax_form.php)
 		        type:     "POST", //метод отправки
 		        dataType: "html", //формат данных
 		        data: $("#"+ajax_form).serialize(),  // Сеарилизуем объект
